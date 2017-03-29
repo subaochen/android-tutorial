6ca6faee7ff3c953e8434dad147bd9af0c7412af
subaochen <subaochen@126.com>
2017-03-29 23:07:25 +0800

    增加startActvityForResult示例，布局还没有调整

---
+++ code/IntentDemo/app/src/main/java/android/softlab/sdut/edu/cn/intentdemo/SubTwoActivity.java
@@ -0,0 +1,28 @@
+package android.softlab.sdut.edu.cn.intentdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SubTwoActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_two);
+
+        button = (Button)findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("msg", "I from SubTwoActivity");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+}
