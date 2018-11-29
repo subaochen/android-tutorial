0ef723d687fe54dbb264560ff3a896bde85951da
subaochen <subaochen@126.com>
2018-11-29 17:19:06 +0800

    add shell to make  book

---
+++ makebook.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+lyx --export pdf4 android.lyx
+#pdftk cover.pdf java-guide-on-c.pdf cat output ../java-guide.pdf
+#pdftk cover-test.pdf tests-guide.pdf cat output ../tests-guide.pdf
+#rm java-guide-on-c.pdf tests-guide.pdf
